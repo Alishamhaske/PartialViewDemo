4e5372f4aca15a15f3db8c97d9c1752e76967bf1
Alishamhaske <mmhaske2001@gmail.com>
2023-10-30 17:37:36 +0530

    Add project files.

---
+++ PartialViewDemo/Data/ApplicationDbContext.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore;
+using PartialViewDemo.Models;
+
+namespace PartialViewDemo.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        //Dbcontextoption is used to override configuration --connection
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> op) : base(op)
+        {
+
+        }
+
+        public DbSet<Book> Books { get; set; }
+        /*public DbSet<Student> Students { get; set; }
+        public DbSet<EmployeeEF> Employees { get; set; }*/
+
+        public static implicit operator ApplicationBuilder(ApplicationDbContext v)
+        {
+            throw new NotImplementedException();
+        }
+
+    }
+
+}
